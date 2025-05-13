3f2505cad4410ebf76cd663817ef6c432d02ae17
albrinBuzz <roman.111.z@protonmail.com>
2025-05-12 21:30:14 -0400

    triger modificado

--- dataBase/trigger.sql
+++ dataBase/trigger.sql
@@ -2,36 +2,35 @@ CREATE OR REPLACE FUNCTION fnActualizarStock()
 RETURNS TRIGGER AS $$
 
 BEGIN
-    -- Verificar si es una inserción (INSERT)
-    IF (TG_OP = 'INSERT') THEN
-        -- Si es una inserción, restamos el stock del producto
-        UPDATE inventario
+
+    IF ( TG_OP = 'INSERT') THEN
+
+            UPDATE inventario
         SET stock = stock - NEW.cantidad
-        WHERE id_producto = NEW.id_producto
+            WHERE  id_producto = NEW.id_producto
           AND id_sucursal = (
-              SELECT id_sucursal
-              FROM pedido
+               SELECT id_sucursal
+              FROM  pedido
               WHERE id_pedido = NEW.id_pedido
           );
 
-    -- Verificar si es una eliminación (DELETE)
     ELSIF (TG_OP = 'DELETE') THEN
-        -- Si es una eliminación, restauramos el stock del producto
-        UPDATE inventario
+
+            UPDATE inventario
         SET stock = stock + OLD.cantidad
-        WHERE id_producto = OLD.id_producto
+        WHERE  id_producto = OLD.id_producto
           AND id_sucursal = (
               SELECT id_sucursal
-              FROM pedido
-              WHERE id_pedido = OLD.id_pedido
+                 FROM pedido
+               WHERE id_pedido = OLD.id_pedido
           );
     END IF;
 
-    RETURN NULL;  -- No necesitamos devolver nada, solo actualizar el stock
+    RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER tr_actualizarStock
-AFTER INSERT OR DELETE ON DetallePedido
+AFTER INSERT  OR DELETE ON DetallePedido
 FOR EACH ROW
 EXECUTE FUNCTION fnActualizarStock();
